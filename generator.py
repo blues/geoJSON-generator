96918eba2f6f1260367b360088573a1a57b05383
Brandon Satrom <bsatrom+gh@gmail.com>
2022-05-06 16:41:50 -0500

    create LineString files for each journey

--- generator.py
+++ generator.py
@@ -10,9 +10,11 @@ def process_json(raw_json):
         "type": "FeatureCollection",
         "features": []
     }
+    journey_map = {}
 
     for event in raw_json:
         if event["file"] == "_track.qo":
+            body = event["body"]
             geo_point = {
                 "type": "Feature",
                 "geometry": {
@@ -29,30 +31,49 @@ def process_json(raw_json):
                     "modified": event["modified"],
                     "where": event["where"],
                     "device": event["device"],
-                    "body": json.dumps(event["body"]),
-                    "value": json.dumps(event["value"])
+                    "body": body,
+                    "value": event["value"]
                 }
             }
+            if body["journey"] not in journey_map:
+                journey_map[body["journey"]] = []
+            journey_map[body["journey"]].append({
+                "jcount": body["jcount"],
+                "coordinates": [
+                    event["value"]["best_lon"],
+                    event["value"]["best_lat"]
+                ]
+            })
             processed_json["features"].append(geo_point)
 
-    return processed_json
+    # order journey_map
+    for journey in journey_map:
+        journey_map[journey] = sorted(journey_map[journey], key=lambda d: d["jcount"])
+        journey_map[journey] = {
+            "type": "LineString",
+            "coordinates": [d["coordinates"] for d in journey_map[journey]]
+        }
+    return processed_json, journey_map
 
-def write_output(events, outfile):
+def write_output(events, journies, outfile):
     with open(outfile,'w',encoding = 'utf-8') as output_file:
         output_file.write(json.dumps(events))
+    for journey in journies:
+        with open(f'geojson-journey-{journey}.json','w',encoding = 'utf-8') as output_file:
+            output_file.write(json.dumps(journies[journey]))
 
 def main():
     parser = argparse.ArgumentParser(description='Process a file of JSON data from Notehub and convert into the geoJSON format.')
     parser.add_argument("-v", "--verbose", help="Enable verbose output", action="store_true")
     parser.add_argument('--infile', metavar='infile.json', type=str, required=True,
                     help='a JSON file with events from Notehub.io')
-    parser.add_argument('--outfile', metavar='outfile.json', default='geojson-out.json',
+    parser.add_argument('--outfile', metavar='outfile.json', default='geojson-points.json',
                     help='the name of a file to output with geoJSON data')
     args = parser.parse_args()
 
     raw_json = fetch_input_json(args.infile)
-    processed_events = process_json(raw_json)
-    write_output(processed_events, args.outfile)
+    processed_events, processed_journies = process_json(raw_json)
+    write_output(processed_events, processed_journies, args.outfile)
 
 
 if __name__ == "__main__":
